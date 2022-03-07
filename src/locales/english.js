e107b74b6727b449f10cd14639a85895fca96d0b
YankaZabka <yanka.zanka@gmail.com>
2022-03-07 14:31:40 +0300

    Add fields to locales

--- src/locales/english.js
+++ src/locales/english.js
@@ -3,7 +3,12 @@ export default {
     formErrors: {
       wrongUrl: 'Link must be a valid URL',
       isUniq: 'RSS already exists',
+      network: "Network response was not ok",
+      parsing: "Xml parsing error"
     },
     formSuccess: 'RSS uploaded successfully',
+    feeds: "Feeds",
+    posts: "Posts",
+    browsing: "Browsing"
   },
 };
--- src/locales/russian.js
+++ src/locales/russian.js
@@ -3,7 +3,12 @@ export default {
     formErrors: {
       wrongUrl: 'Ссылка должна быть валидным URL',
       isUniq: 'RSS уже существует',
+      network: "Проблема с сетью",
+      parsing: "Ошибка парсинга Xml"
     },
     formSuccess: 'Rss успешно загружен',
+    feeds: "Фиды",
+    posts: "Посты",
+    browsing: "Просмотр"
   },
 };
