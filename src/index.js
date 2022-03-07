0001ce07cb98ac1d8ce9ebcc216be89824b45fc5
YankaZabka <yanka.zanka@gmail.com>
2022-03-07 14:53:19 +0300

    Fix index.js and view.js

--- src/index.js
+++ src/index.js
@@ -5,54 +5,62 @@ import i18n from 'i18next';
 import getWatchedState from './view.js';
 import isValidUrl from './yupValidate.js';
 import resources from './locales/index.js';
+import fetchXml from "./fetchXml.js";
 
 const DEFAULT_LANGUAGE = 'ru';
 
 const app = () => {
-  const i18nInstance = i18n.createInstance();
-  i18nInstance.init({
-    lng: DEFAULT_LANGUAGE,
-    debug: false,
-    resources,
-  }).catch((e) => {
-    console.log('Localization error: ', e.message);
-  });
+    const i18nInstance = i18n.createInstance();
+    i18nInstance.init({
+        lng: DEFAULT_LANGUAGE,
+        debug: false,
+        resources,
+    }).catch((e) => {
+        console.log('Localization error: ', e.message);
+    });
 
-  const state = {
-    form: {
-      state: 'empty',
-      feedback: '',
-      error: '',
-    },
-    feeds: [],
-    posts: [],
-  };
+    const state = {
+        form: {
+            state: 'empty',
+            feedback: '',
+            error: '',
+        },
+        feeds: [],
+        posts: [],
+    };
 
-  const formEl = document.querySelector('.rss-form');
-  const inputEl = document.querySelector('#url-input');
-  const feedbackEl = document.querySelector('.feedback');
+    const formEl = document.querySelector('.rss-form');
+    const inputEl = document.querySelector('#url-input');
+    const feedbackEl = document.querySelector('.feedback');
+    const feedsContainer = document.querySelector(".feeds")
+    const postsContainer = document.querySelector(".posts")
 
-  const elements = {
-    formEl,
-    inputEl,
-    feedbackEl,
-  };
+    const elements = {
+        formEl,
+        inputEl,
+        feedbackEl,
+        feedsContainer,
+        postsContainer
+    };
 
-  const watchedState = getWatchedState(state, elements, i18nInstance);
+    const watchedState = getWatchedState(state, elements, i18nInstance);
 
-  formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const formData = new FormData(e.target);
-    const rssUrl = formData.get('url');
+        const formData = new FormData(e.target);
+        const rssUrl = formData.get('url');
 
-    isValidUrl(state.feeds, rssUrl, i18nInstance)
-      .then((response) => {
-        watchedState.feeds.push(response);
-      }).catch((err) => {
-        watchedState.form.error = err.message;
-      });
-  });
+        isValidUrl(state.feeds, rssUrl, i18nInstance)
+            .then(() => fetchXml(state, watchedState, rssUrl, i18nInstance))
+            .catch((err) => {
+            watchedState.form.error = err.message;
+        });
+    });
+
+
 };
 
 app();
+
+
--- src/view.js
+++ src/view.js
@@ -1,6 +1,8 @@
 import onChange from 'on-change';
+import renderPosts from "./components/posts.js"
+import renderFeeds from "./components/feeds.js"
 
-export default (state, { feedbackEl, formEl, inputEl }, i18n) => {
+export default (state, { feedbackEl, formEl, inputEl, feedsContainer, postsContainer}, i18n) => {
   const watchedState = onChange(state, (path, value, previousValue) => {
     console.log('path:', path);
     console.log('value:', value);
@@ -14,12 +16,18 @@ export default (state, { feedbackEl, formEl, inputEl }, i18n) => {
       inputEl.classList.remove('is-invalid');
       formEl.reset();
       inputEl.focus();
+
+      renderFeeds(feedsContainer, value, i18n)
+
     } else if (path === 'form.error') {
       inputEl.classList.add('is-invalid');
       feedbackEl.textContent = value;
 
       feedbackEl.classList.remove('text-success');
       feedbackEl.classList.add('text-danger');
+    } else if (path === "posts") {
+
+      renderPosts(postsContainer, value, i18n)
     }
   });
 
